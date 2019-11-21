eaecc27c73b016dac182471fc89e47bca750869e
Dariusz Okonski <darek200180@gmail.com>
2019-11-21 17:24:50 +0000

    Updates

---
+++ precache-manifest.cd53758b4a775b153f57d2479d9c6f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4fb90c12d286ce85fea544e34c2a35",
+    "url": "/coin-flipper/index.html"
+  },
+  {
+    "revision": "6862d6f60c1019c700d7",
+    "url": "/coin-flipper/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4a00ce574301ff206381",
+    "url": "/coin-flipper/static/js/2.f4d03dd2.chunk.js"
+  },
+  {
+    "revision": "6862d6f60c1019c700d7",
+    "url": "/coin-flipper/static/js/main.8fc3878b.chunk.js"
+  },
+  {
+    "revision": "8d691dbc4afb9fb5f214",
+    "url": "/coin-flipper/static/js/runtime-main.6d15f33f.js"
+  }
+]);
